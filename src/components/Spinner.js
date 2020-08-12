e7537328eee10226d12051d6d44f5d6c02872d05
sujata <gunalesujata@gmail.com>
2020-08-12 20:01:36 +0530

    Github Jobs

---
+++ src/components/Spinner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Spinner() {
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-grow text-danger mr-3" style={{width: '3rem', height: '3rem'}} role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-warning mr-3" style={{width: '3rem', height: '3rem'}} role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-success" style={{width: '3rem', height: '3rem'}} role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    )
+}
+
+export default Spinner
