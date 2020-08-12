5a6ae6af063b145cb0886507e3db0422395d4b48
sujata <gunalesujata@gmail.com>
2020-08-12 23:17:58 +0530

    Updates

---
+++ precache-manifest.36190637f5f50910c5b6637fc93909ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e48f652c16b83bf58dbb2d7b48d651",
+    "url": "/Github-Jobs/index.html"
+  },
+  {
+    "revision": "e04e7c758c4573dfb620",
+    "url": "/Github-Jobs/static/css/main.dc15ec67.chunk.css"
+  },
+  {
+    "revision": "9e91b406199f8a0c1aae",
+    "url": "/Github-Jobs/static/js/2.11ecc17f.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Github-Jobs/static/js/2.11ecc17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04e7c758c4573dfb620",
+    "url": "/Github-Jobs/static/js/main.2401f5be.chunk.js"
+  },
+  {
+    "revision": "c4253f8433f1c38b33e9",
+    "url": "/Github-Jobs/static/js/runtime-main.156afc36.js"
+  },
+  {
+    "revision": "cb222a84de320586f543e17c96133005",
+    "url": "/Github-Jobs/static/media/error.cb222a84.png"
+  }
+]);
